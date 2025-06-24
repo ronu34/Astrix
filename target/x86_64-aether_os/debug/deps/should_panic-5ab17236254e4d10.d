51274985978349a8c5e4247ec681f465aef168db
Ronu Enderman <w24133296@gmail.com>
2025-06-24 21:54:47 +0530

    added tests/should.rs 	modified:   Cargo.toml

---
+++ target/x86_64-aether_os/debug/deps/should_panic-5ab17236254e4d10.d
@@ -0,0 +1,5 @@
+/run/media/ekadrisarkar/Ventoy/astrix/target/x86_64-aether_os/debug/deps/should_panic-5ab17236254e4d10.d: tests/should_panic.rs
+
+/run/media/ekadrisarkar/Ventoy/astrix/target/x86_64-aether_os/debug/deps/libshould_panic-5ab17236254e4d10.rmeta: tests/should_panic.rs
+
+tests/should_panic.rs:
---
+++ target/x86_64-aether_os/debug/deps/should_panic-d98de57a85a9e22e.d
@@ -0,0 +1,5 @@
+/run/media/ekadrisarkar/Ventoy/astrix/target/x86_64-aether_os/debug/deps/should_panic-d98de57a85a9e22e.d: tests/should_panic.rs
+
+/run/media/ekadrisarkar/Ventoy/astrix/target/x86_64-aether_os/debug/deps/should_panic-d98de57a85a9e22e: tests/should_panic.rs
+
+tests/should_panic.rs:
